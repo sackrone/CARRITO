69e359ccd85d13d83640b8af76b6f5fc80194413
Carlos Lomeli <sackrone@hotmail.com>
2022-04-21 02:32:02 -0500

    20220421_01

---
+++ Carrito.Entidad/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace Carrito.Entidad
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public bool Activo { get; set; }
+    }
+}
